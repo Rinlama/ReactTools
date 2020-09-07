22ef574d767dbe6bbe561355021edb7e3c3e8e37
rinlama <rinjinlama83@gmail.com>
2020-09-06 22:35:46 -0400

    use ref

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(ref.current);
+      ref.current.style.border = "solid 2px red";
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={ref}></input>
     </div>
   );
 }
