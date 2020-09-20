188e1061eab981506d572f94d93be7ce6c840b31
rinlama <rinjinlama83@gmail.com>
2020-09-20 00:29:27 -0400

    fileUpload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const postFile = (file) => {
+    const formdata = new FormData();
+    formdata.append("file", file);
+    postFiles(formdata, "file");
+  };
+
+  const postMultipleFile = (filelist) => {
+    const formdata = new FormData();
+    for (let index = 0; index < filelist.length; index++) {
+      const file = filelist[index];
+      formdata.append("files", file);
+    }
+    postFiles(formdata, "multifiles");
+  };
+
+  const postFiles = async (formdata, endpoint) => {
+    try {
+      const result = await axios.post(
+        `http://localhost:4000/${endpoint}`,
+        formdata
+      );
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        onChange={(event) => {
+          const file = event.target.files[0];
+          postFile(file);
+        }}
+      ></input>
+
+      <input
+        type="file"
+        multiple
+        onChange={(event) => {
+          const filelist = event.target.files;
+          postMultipleFile(filelist);
+        }}
+      ></input>
     </div>
   );
 }
